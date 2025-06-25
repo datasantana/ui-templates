b2061a7748441ba2d9498815248da30640f1b04b
datasantana <juan.linares@geostudio.com.co>
2025-06-25 15:07:05 -0500

    Add Vue Router and multiple views for navigation

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+    <HelloWorld msg="Vite + Vue"/>
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld.vue';
+export default {
+    name: 'Home',
+    components: {
+        HelloWorld
+    },
+    data() {
+        return {
+            message: 'This is Home Page',
+        };
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/MapView.vue
@@ -0,0 +1,22 @@
+<template>
+    <HelloWorld msg="Map"/>
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld.vue';
+export default {
+    name: 'MapView',
+    components: {
+        HelloWorld
+    },
+    data() {
+        return {
+            message: 'This is Map Page',
+        };
+    }
+}
+</script>
+
+<style>
+
+</style>
